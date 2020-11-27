eabc9a1c7a7c7879cf363fdbef069f98d1d0905d
Noah Sandman <nsandman09@gmail.com>
2020-11-27 15:15:52 -0800

    initial commit with project config

---
+++ src/main/java/com/modulytic/dalia/Main.java
@@ -0,0 +1,9 @@
+package com.modulytic.dalia;
+
+import net.gescobar.smppserver.SmppServer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Goodbye cruel world ...");
+    }
+}
